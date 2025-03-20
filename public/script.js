fa049ddac01c83d08c28e9fc235d0fd1bc462c59
yamato-428 <pfd34pam@gmail.com>
2025-03-20 12:34:22 +0000

    gfs

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@
 const SUPABASE_URL = "https://gflvuocpcuiootlumzte.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmbHZ1b2NwY3Vpb290bHVtenRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzOTk0NzMsImV4cCI6MjA1Nzk3NTQ3M30.Psyu6o5j_HbG1kJhqrnnhrnbznArH3JWAE_tJEKdPuA"; // あなたのAPIキー
 const BUCKET_NAME = "backups"; // バケット名
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // ファイル選択後の処理
 document.getElementById("fileInput").addEventListener("change", async function (event) {
